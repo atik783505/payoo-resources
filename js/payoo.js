72fb5fae8a630237a2c4ef9673dca0d839096936
atik783505 <atik13672@gmail.com>
2026-02-18 11:09:59 +0600

    change

--- js/payoo.js
+++ js/payoo.js
@@ -5,7 +5,7 @@ document.getElementById('login-btn').addEventListener('click', function(){
     const newPin = loginPin.value;
     if(newNum == '01600690954' && newPin == '5263') {
         alert('login succecful');
-        window.location.assign('/home.html')
+        window.location.assign('home.html')
     }else{
         alert('login failed')
         return;
